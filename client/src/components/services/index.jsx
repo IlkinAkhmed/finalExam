eafeec23c8b16a740d0cf3b8d6cd1bb86d7e642d
IlkinAkhmed <tu7mp4w99@code.edu.az>
2024-01-25 17:41:32 +0400

    last commit

--- client/src/components/services/index.jsx
+++ client/src/components/services/index.jsx
@@ -5,13 +5,13 @@ function Services() {
   return (
     <div className="services">
       <div className="ser-head">
-        <i class="fa-solid fa-bell-concierge"></i>
+        <i className="fa-solid fa-bell-concierge"></i>
         <div className="line"></div>
         <h2>Our Services</h2>
       </div>
       <div className="ser-wrapper">
         <div className="ser-card">
-          <i class="fa-solid fa-mug-hot"></i>
+          <i className="fa-solid fa-mug-hot"></i>
           <h3>Breakfast</h3>
           <p>
             In vitae nisi aliquam, scelerisque leo a, volutpat sem. Vivamus
@@ -19,7 +19,7 @@ function Services() {
           </p>
         </div>
         <div className="ser-card">
-          <i class="fa-solid fa-cake-candles"></i>
+          <i className="fa-solid fa-cake-candles"></i>
           <h3>Dinner</h3>
           <p>
             In vitae nisi aliquam, scelerisque leo a, volutpat sem. Vivamus
@@ -27,7 +27,7 @@ function Services() {
           </p>
         </div>
         <div className="ser-card">
-          <i class="fa-solid fa-mug-hot"></i>
+          <i className="fa-solid fa-mug-hot"></i>
           <h3>Lunch</h3>
           <p>
             In vitae nisi aliquam, scelerisque leo a, volutpat sem. Vivamus
